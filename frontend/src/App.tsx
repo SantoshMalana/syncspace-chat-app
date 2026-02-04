f433325eab104f4e3985fc364d9267620e4a20e1
santosh malana <150065221+SantoshMalana@users.noreply.github.com>
2026-02-04 18:27:37 +0530

    Fix Tailwind config and add signup/login navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/signup" element={<Signup />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
